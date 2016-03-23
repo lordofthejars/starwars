32c3e8345f0b3863199f293beb5ddab79f9a8ac1
Alex Soto <asotobueno@cloudbees.com>
2016-03-23 15:27:31 +0100

    Add contract testing at Jenkins

--- jenkins/pact.groovy
+++ jenkins/pact.groovy
@@ -1,18 +1,20 @@
 def withRequiredPacts(body) {
 
+    def pactFolders;
     dir('build') {
         git 'file:///Users/alexsoto/git/starwars_pacts'
+        pactFolders = findFiles()
     }
 
     def workspace = pwd()
-    def pactDirectories = findFiles()
-    echo "*** ${pactDirectories.size()}"
-    for(int i = 0; i < pactDirectories.length; i++) {
-        echo ">> ${pactDirectories[i].directory} + ${pactDirectories[i].path}"
-        if (pactDirectories[i].directory && pactDirectories[i].path.endsWith('_planets_provider')) {
-            def pactDirectory = "${workspace}/${pactDirectories[i].path}"
+
+    echo "*** ${pactFolders.size()}"
+    for(int i = 0; i < pactFolders.length; i++) {
+        echo ">> ${pactFolders[i].directory} + ${pactFolders[i].path}"
+        if (pactFolders[i].directory && pactFolders[i].path.endsWith('_planets_provider')) {
+            def pactDirectory = "${workspace}/build/${pactFolders[i].path}"
             withEnv(["pacts=${pactDirectory}"]) {
-                println "Pact found at ${pactDirectories[i].path}"
+                println "Pact found at ${pactFolders[i].path}"
                 body.call
             }
         }
