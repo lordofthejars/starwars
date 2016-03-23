204d6e0b8abd98fca08108c1f3ef22156f12425a
Alex Soto <asotobueno@cloudbees.com>
2016-03-23 15:52:13 +0100

    Add contract testing at Jenkins

--- jenkins/pact.groovy
+++ jenkins/pact.groovy
@@ -1,16 +1,14 @@
 def withRequiredPacts(body) {
 
-    def pactFolders;
     dir('build') {
         git 'file:///Users/alexsoto/git/starwars_pacts'
     }
 
     def workspace = pwd()
-    pactFolders = findFiles(glob: 'build/**')
-    echo "*** ${pactFolders.size()}"
+    def pactFolders = findFiles(glob: 'build/**')
     for(int i = 0; i < pactFolders.length; i++) {
         echo ">> ${pactFolders[i].directory} + ${pactFolders[i].path}"
-        if (pactFolders[i].directory && pactFolders[i].path.endsWith('_planets_provider')) {
+        if (pactFolders[i].directory && pactFolders[i].path.endsWith('_planets_provider/')) {
             def pactDirectory = "${workspace}/build/${pactFolders[i].path}"
             withEnv(["pacts=${pactDirectory}"]) {
                 println "Pact found at ${pactFolders[i].path}"
